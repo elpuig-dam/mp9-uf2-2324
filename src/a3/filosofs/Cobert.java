16c6058848ad3f59906b970408733ca9cff1bf64
jherna <jherna76@elpuig.xeill.net>
2023-11-15 19:11:10 +0100

    add filosofs exercise

---
+++ src/a3/filosofs/Cobert.java
@@ -0,0 +1,37 @@
+package a3.filosofs;
+
+public class Cobert {
+
+	public enum estatCobert {
+		LLIURE, OCUPAT
+	}
+	
+	private  Enum<estatCobert> estat;
+	
+	public Cobert () {
+		estat = estatCobert.LLIURE;
+	}
+
+	public synchronized Enum<estatCobert> getEstat() {
+		return estat;
+	}
+
+	public synchronized void agafar() {
+		try {
+			
+			while (estat == estatCobert.OCUPAT) wait();
+			estat = estatCobert.OCUPAT;
+			notifyAll();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public synchronized void deixar() {
+		estat = estatCobert.LLIURE;
+		notifyAll();
+	}
+
+}
